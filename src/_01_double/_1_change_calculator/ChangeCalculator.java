56398d49c5f682b37bc8197897e9c37e2d76787b
ryansmith <ryansmith@ryans-air.lan1>
2023-01-23 18:26:39 -0800

    Change Calculator

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -11,6 +11,27 @@ public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
+		String stringPen = JOptionPane.showInputDialog(null, "How many pennies do you have?");
+		int intPen = Integer.parseInt(stringPen);
+		
+		String stringNic = JOptionPane.showInputDialog(null, "How many nickels do you have?");
+		int intNic = Integer.parseInt(stringNic);
+		
+		String stringDim = JOptionPane.showInputDialog(null, "How many dimes do you have?");
+		int intDim = Integer.parseInt(stringDim);
+		
+		String stringQua = JOptionPane.showInputDialog(null, "How many quarters do you have?");
+		int intQua = Integer.parseInt(stringQua);
+		
+		int valPen = 1;
+		
+		
+		int ans = intPen ;
+				
+				//+ (intNic*0.05) + (intDim*0.1) + (intQua*0.25);
+		
+		
+		
 		// Ask the user how many nickels they have
 
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
